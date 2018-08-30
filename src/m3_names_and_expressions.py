86563d994686f3799de2121ab9a50b4032ec3771
johnsokk <johnsokk@rose-hulman.edu>
2018-08-30 10:27:00 -0400

    Welcome to CSSE 120

--- src/m3_names_and_expressions.py
+++ src/m3_names_and_expressions.py
@@ -34,16 +34,18 @@ print(math.sqrt(answer))
 ###############################################################################
 
 ###############################################################################
-# TODO: 3.
+# DONE: 3.
 #   Immediately below this _TODO_, write code that computes and prints:
 #      the square root of ((41 * 88) + (4 * the cosine of 2))
 #   Use as few or as many intermediate names as you feel appropriate.
 #   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
 ###############################################################################
+print(math.sqrt((41*81)+(4*math.cos(2))))
+
 
 
 ###############################################################################
-# TODO: 4.
+# DONE: 4.
 #   Immediately below this _TODO_,
 #   write code that computes the square root of 2 in two ways:
 #     - By using the   math.sqrt   function.
@@ -51,10 +53,12 @@ print(math.sqrt(answer))
 #   Print both of the expressions that you write.
 #   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
 ###############################################################################
+print(math.sqrt(2))
+print(2**.5)
 
 
 ###############################################################################
-# TODO: 5.
+# DONE: 5.
 #   Every object has a TYPE and a VALUE.  For example,
 #   for the object that is computed by  math.sqrt(2):
 #      Its TYPE is float  (which is shorthand for "floating point number")
@@ -92,4 +96,18 @@ print(math.sqrt(answer))
 #
 # After you have written and run the code to learn the TYPE
 # of each of the above, change the above _TODO_ to DONE.
+print(type(3.14))
+print(type("hello"))
+print(type('hello'))
+print(type("a b c"))
+print(type(3+3))
+print(type("3"+"3"))
+print(type(2**100))
+print(type(2.0**100))
+print(type(math.sin(8)))
+print(type(math.sin()))
+print(type(print))
+print(type(math))
+print(type('math'))
+
 ###############################################################################
